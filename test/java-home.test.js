f4058c06e1fbacc76701be4b69cd0fe4582648ae
zenzhu <zic.zhu@gmail.com>
2016-04-07 15:15:30 +0800

    Refactor and remove deps

--- test/java-home.test.js
+++ test/java-home.test.js
@@ -13,7 +13,28 @@
 
 'use strict';
 
-var java-home = require('../');
+var javaHome = require('../');
 
 describe('test', function(){
+  it('getPath should be a promise', function(done) {
+    javaHome.getPath().then(javaHomePath => {
+      console.log(javaHomePath);
+      done();
+    }, err => {
+      console.log(err);
+      done();
+    });
+  });
+
+  it('getPath should accept callback', function(done) {
+    javaHome.getPath((err, javaHomePath) => {
+      if (err) {
+        console.log(err);
+        done();
+      } else {
+        console.log(javaHomePath);
+        done();
+      }
+    });
+  });
 });
